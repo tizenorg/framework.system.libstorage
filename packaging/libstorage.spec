771491b504c9fe2749354c58193c7e36c82fe616
jk7744.park <jk7744.park@samsung.com>
2015-09-08 22:38:01 +0900

    tizen 2.3.1 release

---
+++ packaging/libstorage.spec
@@ -0,0 +1,57 @@
+Name:       libstorage
+Summary:    Library to get storage information
+Version:    0.1.0
+Release:    0
+Group:      System/Libraries
+License:    Apache-2.0
+Source0:    %{name}-%{version}.tar.gz
+Source1:    %{name}.manifest
+BuildRequires:  cmake
+BuildRequires:  pkgconfig(dlog)
+BuildRequires:  pkgconfig(capi-base-common)
+BuildRequires:  pkgconfig(vconf)
+BuildRequires:  pkgconfig(glib-2.0)
+
+%description
+development package of library to get storage
+
+%package devel
+Summary:	Get storage information (devel)
+Group:		Development/Libraries
+Requires:	%{name} = %{version}-%{release}
+
+%description devel
+Library to get storage information (devel)
+
+
+%prep
+%setup -q
+
+%build
+%if 0%{?tizen_build_binary_release_type_eng}
+export CFLAGS+=" -DTIZEN_ENGINEER_MODE"
+%endif
+CFLAGS="$CFLAGS" cmake . -DCMAKE_INSTALL_PREFIX=%{_prefix}
+make %{?jobs:-j%jobs}
+
+%install
+rm -rf %{buildroot}
+mkdir -p %{buildroot}/usr/share/license
+cp LICENSE %{buildroot}/usr/share/license/%{name}
+%make_install
+cp -a %{SOURCE1} %{buildroot}%{_datadir}/
+
+%post
+
+%postun
+
+%files
+%{_libdir}/*.so.*
+%{_libdir}/pkgconfig/libstorage.conf
+%{_datadir}/license/%{name}
+%manifest %{_datadir}/%{name}.manifest
+
+%files devel
+%{_includedir}/storage/*.h
+%{_libdir}/*.so
+%{_libdir}/pkgconfig/*.pc
---
+++ test/testcase/utc_system_storage.c
@@ -0,0 +1,74 @@
+/*
+ *
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ * PROPRIETARY/CONFIDENTIAL
+ *
+ * This software is the confidential and proprietary information of SAMSUNG
+ * ELECTRONICS ("Confidential Information"). You agree and acknowledge that
+ * this software is owned by Samsung and you shall not disclose such
+ * Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with SAMSUNG ELECTRONICS. SAMSUNG
+ * make no representations or warranties about the suitability of the software,
+ * either express or implied, including but not limited to the implied
+ * warranties of merchantability, fitness for a particular purpose, or
+ * non-infringement. SAMSUNG shall not be liable for any damages suffered by
+ * licensee arising out of or related to this software.
+ *
+ */
+#include <tet_api.h>
+#include <storage.h>
+
+#define API_NAME_STORAGE_GET_INTERNAL_MEMORY_SIZE "storage_get_internal_memory_size"
+
+static void startup(void);
+static void cleanup(void);
+
+void (*tet_startup)(void) = startup;
+void (*tet_cleanup)(void) = cleanup;
+
+
+static void utc_system_storage_get_internal_memory_size_p(void);
+static void utc_system_storage_get_internal_memory_size_n(void);
+
+enum {
+	POSITIVE_TC_IDX = 0x01,
+	NEGATIVE_TC_IDX,
+};
+
+struct tet_testlist tet_testlist[] = {
+	{ utc_system_storage_get_internal_memory_size_p, POSITIVE_TC_IDX },
+	{ utc_system_storage_get_internal_memory_size_n, NEGATIVE_TC_IDX },
+	{ NULL, 0 },
+};
+
+
+static void startup(void)
+{
+}
+
+static void cleanup(void)
+{
+}
+
+/**
+ * @brief Positive test case of storage_get_internal_memory_size()
+ */
+static void utc_system_storage_get_internal_memory_size_p(void)
+{
+	struct statvfs s;
+	int ret;
+
+	ret = storage_get_internal_memory_size(&s);
+	dts_check_eq(API_NAME_STORAGE_GET_INTERNAL_MEMORY_SIZE, ret, 0);
+}
+
+/**
+ * @brief Negative test case of storage_get_internal_memory_size()
+ */
+static void utc_system_storage_get_internal_memory_size_n(void)
+{
+	int ret;
+
+	ret = storage_get_internal_memory_size(NULL);
+	dts_check_ne(API_NAME_STORAGE_GET_INTERNAL_MEMORY_SIZE, ret, 0);
+}
